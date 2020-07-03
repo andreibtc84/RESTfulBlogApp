7c451919a7ceb2849389e3e2c07c08dc652461b3
root <andreibtc@gmail.com>
2020-07-03 11:35:19 +0000

    Added EDIT and UPDATE post functionality

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var methodOverride = require("method-override");
 var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require("mongoose");
@@ -9,6 +10,7 @@ mongoose.connect("mongodb://localhost/restful_blog_app", {useNewUrlParser: true,
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
 
 
 // Mongoose/model config
@@ -77,7 +79,29 @@ app.get("/blogs/:id", function(req, res){
 	});
 });
 
+// EDIT ROUTE
+app.get("/blogs/:id/edit", function(req, res){
+	Blog.findById(req.params.id, function(err, foundBlog){
+		if(err){
+			res.redirect("/blogs");
+		} else {
+			res.render("edit", {blog: foundBlog});
+		}
+	});
+	
+});
 
+// UPDATE
+
+app.put("/blogs/:id", function(req, res){
+	Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
+		if(err){
+			res.redirect("/blogs");
+		} else {
+			res.redirect("/blogs/" + req.params.id);
+		}
+	});
+});
 
 
 
